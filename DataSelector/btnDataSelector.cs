5f875f8d21423373082c1a8dae1558f8f0e4e1d6
Andy Foy <andy@andyfoyconsulting.co.uk>
2024-01-09 09:40:03 +0000

    Initial commit

---
+++ DataSelector/btnDataSelector.cs
@@ -0,0 +1,34 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using DataSelector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSelector
+{
+	internal class btnDataSelector : Button
+{
+    protected override void OnClick()
+    {
+            string stringURI = ArcGIS.Desktop.Core.Project.Current.URI;
+            ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show($"Project path:  {stringURI}", "Project Info");
+
+            //Window1 frmMyForm = new Window1();
+            //frmMyForm.ShowDialog();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DataSelector-ArcPro
+ 
